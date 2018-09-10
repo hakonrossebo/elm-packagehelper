43a525daa0805f7c84d1bca8dab459c68185bb93
Håkon Rossebø <hakonrossebo@hotmail.com>
2018-09-11 00:09:08 +0200

    Handle new package version list

--- PackagesCache/Program.fs
+++ PackagesCache/Program.fs
@@ -1,37 +1,20 @@
 ﻿open System.Net;
 open FSharp.Data;
 
-let allPackages = JsonValue.Load(__SOURCE_DIRECTORY__ + "/data/all-packages.json")
 
-let allPackages' = allPackages.Properties()
+type SearchJsonPackages = JsonProvider<"data/search.json">
 
+let packages = SearchJsonPackages.GetSamples()
 
-let countReleases v = 
-    match v with
-    | JsonValue.Array arr ->
-        Array.length arr
-    | _ -> 0
-
-let getLastRelease v = 
-    match v with
-    | JsonValue.Array arr ->
-        let last = 
-            arr |> Array.last
-        match last with 
-        | JsonValue.String str ->
-            str 
-        | _ -> "Verson not a string"
-    | _ -> "No version"
-
-
+let packagesAndLastVersion = packages
+                            |> Seq.map (fun x -> (x.Name, Array.last x.Versions.Strings))
+                            |> Seq.take 5
 
 
 [<EntryPoint>]
 let main argv =
     // let wc = new WebClient()
     // wc.DownloadFile("https://package.elm-lang.org/packages/krisajenkins/remotedata/latest/docs.json", @"c:\temp\remotedatadocs.json")
-    // Seq.iter (fun (k, v)  -> printfn "%s - %s" k (getLastRelease v)) allPackages'
-    allPackages'
-    |> Seq.map (fun (k, v)  -> (k, (getLastRelease v)))
+    packagesAndLastVersion
     |> Seq.iter (fun (name, release) -> printfn "%s - %s" name release)
     0
