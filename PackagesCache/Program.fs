d2629d61e4fb7aa2ab78966dd57509b80693dde2
Håkon Rossebø <hakonrossebo@hotmail.com>
2018-09-10 15:57:53 +0200

    Initial commit

---
+++ PackagesCache/Program.fs
@@ -0,0 +1,16 @@
+﻿open System.Net;
+open FSharp.Data;
+
+type AllPackages = JsonProvider<"data/all-packages.json">
+let packages = AllPackages.GetSample()
+
+let x = packages.``1602ElmFeather``.Strings
+
+
+
+[<EntryPoint>]
+let main argv =
+    // let wc = new WebClient()
+    // wc.DownloadFile("https://package.elm-lang.org/packages/krisajenkins/remotedata/latest/docs.json", @"c:\temp\remotedatadocs.json")
+    x.Length |> printfn "%d"
+    0
