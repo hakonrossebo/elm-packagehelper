9175bfa87188ed0c1cbfe2a19d5c684bdbbe0f5e
Håkon Rossebø <hakonrossebo@hotmail.com>
2018-09-13 22:29:26 +0200

    Initial Elm project

---
+++ elm-docs/src/Main.elm
@@ -0,0 +1,18 @@
+module Main exposing (..)
+
+import Elm.Docs exposing (..)
+import Json.Decode exposing (..)
+import SmallDocs exposing (..)
+
+
+listModules =
+    case decodeElmCoreModules of
+        Ok modules ->
+            List.map (\x -> x.name) modules
+
+        Err err ->
+            [ Json.Decode.errorToString err ]
+
+
+decodeElmCoreModules =
+    Json.Decode.decodeString (Json.Decode.list decoder) smallDocs
---
+++ elm-docs/src/SmallDocs.elm
@@ -0,0 +1,7 @@
+module SmallDocs exposing (..)
+
+
+smallDocs =
+    """
+[{"name":"Payment.CreditCard.Validation","comment":"","unions":[],"aliases":[],"values":[{"name":"isValid","comment":"","type":"String.String -> Basics.Bool"}],"binops":[]}]
+"""
