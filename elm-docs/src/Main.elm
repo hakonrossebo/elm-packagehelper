1c6fd01f6142ec038fe1c05ef819ad0488bfac9f
Håkon Rossebø <hakonrossebo@hotmail.com>
2018-09-15 23:42:49 +0200

    Get package info

--- elm-docs/src/Main.elm
+++ elm-docs/src/Main.elm
@@ -1,19 +1,71 @@
 module Main exposing (..)
 
+import Elm.Docs
 import Generated.AllElmDocsDecoders exposing (..)
 import Json.Decode exposing (..)
 
 
-searchModules searchArg =
+-- searchModules searchArg =
+--     decoderList
+--         |> List.filter (\( packageName, _ ) -> String.contains searchArg packageName)
+--         |> List.map (\( packageName, _ ) -> packageName)
+
+
+type
+    FindPackageResult
+    -- = Found (List Elm.Docs.Module)
+    = Found String String (List String)
+    | FoundEmptyList
+    | FoundWithDecoderError String
+    | MultipleFound String (List String)
+    | NotFound String
+
+
+decodeAllModules =
     decoderList
-        |> List.filter (\( packageName, _ ) -> String.contains searchArg packageName)
-        |> List.map (\( packageName, _ ) -> packageName)
+        |> List.map (\( packageName, packageDecoder ) -> ( packageName, packageDecoder ))
+        |> List.length
 
 
+getPackageInfo findPackageName =
+    let
+        matchingPackages =
+            decoderList
+                |> List.filter (\( packageName, _ ) -> String.contains findPackageName packageName)
 
--- listModules =
---     case decode_p_elm_core of
---         Ok modules ->
---             List.map (\x -> x.name) modules
---         Err err ->
---             [ Json.Decode.errorToString err ]
+        package =
+            case List.length matchingPackages of
+                0 ->
+                    NotFound "Could not find any matching packages"
+
+                1 ->
+                    matchingPackages
+                        |> List.head
+                        |> decodeIfFound
+
+                _ ->
+                    matchingPackages
+                        |> List.map (\( packageName, _ ) -> packageName)
+                        |> MultipleFound "Found multiple matching packages"
+    in
+    package
+
+
+decodeIfFound maybeDecoder =
+    case maybeDecoder of
+        Just packageInfo ->
+            runDecoder packageInfo
+
+        Nothing ->
+            FoundEmptyList
+
+
+runDecoder ( packageName, decoder ) =
+    case decoder of
+        Ok modules ->
+            modules
+                |> List.map (\packageModule -> packageModule.name)
+                |> Found packageName "Modules:"
+
+        Err err ->
+            FoundWithDecoderError (Json.Decode.errorToString err)
